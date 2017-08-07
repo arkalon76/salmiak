194a1d9cd297deae5a5695318a7652afbdb0c398
Kenth Fagerlund <kenth@cherrypicks.com>
2017-08-07 14:26:04 +0800

    Bumpted up the test coverage to 66%. Not great but better than 65%...

--- tests/test_salmiak.py
+++ tests/test_salmiak.py
@@ -33,3 +33,69 @@ def test_valid_filename(test_file, expected, tmpdir):
 def test_valid_path(test_path, expected, tmpdir):
     f1 = tmpdir.mkdir(test_path)
     assert salmiak.isValidPath(str(f1)) == expected
+
+
+@pytest.mark.parametrize("test_file, renamed_file", [
+    ('Kimi.no.na.wa.aka.Your.Name.2016.JAPANESE.1080p.BluRay.REMUX.AVC.DTS-HD.MA.5.1-FGT.mkv', 'Kimi no na wa aka Your Name (2016).mkv'),
+    ('The.Princess.And.The.Frog.2009.1080p.BluRay.AVC.DTS-HD.MA.5.1-FGT.mkv', 'The Princess And The Frog (2009).mkv'),
+])
+
+
+def test_rename_file(test_file, renamed_file, tmpdir):
+    ''' Testing that renaming a valid file actually moves it from one to the other.
+    '''
+    salmiak.DRYRUN = False
+    f1 = tmpdir.mkdir('download').join(test_file)
+    f1.write('VideoContent')
+    salmiak.renameFile(str(tmpdir) + '/download', str(test_file))
+    assert os.path.isfile(str(tmpdir) + '/download/' + renamed_file) is True
+    assert os.path.isfile(str(tmpdir) + '/download/' + test_file) is False
+
+
+@pytest.mark.parametrize("test_file, renamed_file", [
+    ('Kimi.no.na.wa.aka.Your.Name.2016.JAPANESE.1080p.BluRay.REMUX.AVC.DTS-HD.MA.5.1-FGT.mkv', 'Kimi no na wa aka Your Name (2016).mkv'),
+    ('The.Princess.And.The.Frog.2009.1080p.BluRay.AVC.DTS-HD.MA.5.1-FGT.mkv', 'The Princess And The Frog (2009).mkv'),
+])
+
+
+def test_dryrun_rename_file(test_file, renamed_file, tmpdir):
+    ''' Dry run should not move the files
+    '''
+    salmiak.DRYRUN = True
+    f1 = tmpdir.mkdir('download').join(test_file)
+    f1.write('VideoContent')
+    salmiak.renameFile(str(tmpdir) + '/download', str(test_file))
+    assert os.path.isfile(str(tmpdir) + '/download/' + renamed_file) is False
+    assert os.path.isfile(str(tmpdir) + '/download/' + test_file) is True
+
+
+@pytest.mark.parametrize("test_folder, renamed_folder", [
+    ('Kimi.no.na.wa.aka.Your.Name.2016.JAPANESE.1080p.BluRay.REMUX.AVC.DTS-HD.MA.5.1-FGT', 'Kimi no na wa aka Your Name (2016)'),
+    ('The.Princess.And.The.Frog.2009.1080p.BluRay.AVC.DTS-HD.MA.5.1-FGT', 'The Princess And The Frog (2009)'),
+])
+
+
+def test_rename_folder(test_folder, renamed_folder, tmpdir):
+    ''' Testing that renaming a valid file actually moves it from one to the other.
+    '''
+    salmiak.DRYRUN = False
+    f1 = tmpdir.mkdir(test_folder)
+    salmiak.renamePath(str(tmpdir), str(test_folder))
+    assert os.path.isdir(str(tmpdir) + '/' + renamed_folder) is True
+    assert os.path.isdir(str(tmpdir) + '/' + test_folder) is False
+
+
+@pytest.mark.parametrize("test_folder, renamed_folder", [
+    ('Kimi.no.na.wa.aka.Your.Name.2016.JAPANESE.1080p.BluRay.REMUX.AVC.DTS-HD.MA.5.1-FGT', 'Kimi no na wa aka Your Name (2016)'),
+    ('The.Princess.And.The.Frog.2009.1080p.BluRay.AVC.DTS-HD.MA.5.1-FGT', 'The Princess And The Frog (2009)'),
+])
+
+
+def test_dryrun_rename_folder(test_folder, renamed_folder, tmpdir):
+    ''' Testing that renaming a valid file actually moves it from one to the other.
+    '''
+    salmiak.DRYRUN = True
+    f1 = tmpdir.mkdir(test_folder)
+    salmiak.renamePath(str(tmpdir), str(test_folder))
+    assert os.path.isdir(str(tmpdir) + '/' + renamed_folder) is False
+    assert os.path.isdir(str(tmpdir) + '/' + test_folder) is True
